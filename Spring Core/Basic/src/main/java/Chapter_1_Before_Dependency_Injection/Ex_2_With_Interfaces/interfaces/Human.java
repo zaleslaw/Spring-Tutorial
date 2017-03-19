3adeffb3f4aa0ef615e46fbc496fdfe9890ebcf1
zaleslaw <zaleslaw.sin@gmail.com>
2017-03-19 18:22:58 +0300

    Rename

--- Spring Core/Basic/src/main/java/Chapter_1_Before_Dependency_Injection/Ex_2_With_Interfaces/interfaces/Human.java
+++
@@ -1,8 +0,0 @@
-package Chapter_1_Before_Dependency_Injection.Ex_2_With_Interfaces.interfaces;
-
-/**
- * Created by Alexey_Zinovyev on 18-Mar-17.
- */
-public interface Human {
-    void speak();
-}
